f546ed566eb81b3db16e9f1c353443eafddb1bcc
kaviyarsips <kavibhavi2006@gmail.com>
2026-07-06 13:55:28 +0530

    Add files via upload

---
+++ Week-1/Algorithms_Data Structures/Exercise 7: Financial Forecasting/FinancialForecast.java
@@ -0,0 +1,14 @@
+package com.cognizant.forecasting;
+
+public class FinancialForecast {
+
+    // Recursive method to calculate future value
+    public static double futureValue(double presentValue, double growthRate, int years) {
+
+        if (years == 0) {
+            return presentValue;
+        }
+
+        return futureValue(presentValue, growthRate, years - 1) * (1 + growthRate);
+    }
+}
