5759b6ddca4cf2bfd0a156e7fea882c462f7eeac
kaviyarsips <kavibhavi2006@gmail.com>
2026-07-06 17:47:13 +0530

    Add files via upload

--- Week-1/1. JUnit_Basic Testing Exercises/Exercise 3: Assertions in JUnit/AssertionsTest.java
+++ Week-1/1. JUnit_Basic Testing Exercises/Exercise 3: Assertions in JUnit/AssertionsTest.java
@@ -5,59 +5,21 @@ import org.junit.jupiter.api.Test;
 public class AssertionsTest {
 
     @Test
-    void testAssertEquals() {
+    void testAssertions() {
 
-        int result = 10 + 20;
+        // Assert Equals
+        assertEquals(5, 2 + 3);
 
-        assertEquals(30, result);
+        // Assert True
+        assertTrue(5 > 3);
 
+        // Assert False
+        assertFalse(5 < 3);
+
+        // Assert Null
+        assertNull(null);
+
+        // Assert Not Null
+        assertNotNull(new Object());
     }
-
-    @Test
-    void testAssertTrue() {
-
-        int age = 20;
-
-        assertTrue(age >= 18);
-
-    }
-
-    @Test
-    void testAssertFalse() {
-
-        int age = 15;
-
-        assertFalse(age >= 18);
-
-    }
-
-    @Test
-    void testAssertNull() {
-
-        String name = null;
-
-        assertNull(name);
-
-    }
-
-    @Test
-    void testAssertNotNull() {
-
-        String name = "Kaviyarasi";
-
-        assertNotNull(name);
-
-    }
-
-    @Test
-    void testAssertThrows() {
-
-        assertThrows(ArithmeticException.class, () -> {
-
-            int result = 10 / 0;
-
-        });
-
-    }
-
 }
