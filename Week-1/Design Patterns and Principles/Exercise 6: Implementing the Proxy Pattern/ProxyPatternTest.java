ea6d2009ef429fc9dc842486e1dda22ce6941be4
kaviyarsips <kavibhavi2006@gmail.com>
2026-07-06 13:34:34 +0530

    Add files via upload

---
+++ Week-1/Design Patterns and Principles/Exercise 6: Implementing the Proxy Pattern/ProxyPatternTest.java
@@ -0,0 +1,20 @@
+package com.cognizant.proxy;
+
+public class ProxyPatternTest {
+
+    public static void main(String[] args) {
+
+        Image image = new ProxyImage("Nature.jpg");
+
+        System.out.println("First Display:");
+
+        image.display();
+
+        System.out.println();
+
+        System.out.println("Second Display:");
+
+        image.display();
+
+    }
+}
