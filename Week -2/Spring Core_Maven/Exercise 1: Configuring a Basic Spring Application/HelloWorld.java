5063e8179c0be958cfc70e60b36de08174cf4323
kaviyarsips <kavibhavi2006@gmail.com>
2026-07-06 19:57:08 +0530

    Delete Week -2/Spring Core_Maven/Exercise 1: Configuring a Basic Spring Application/HelloWorld.java
    New file has updated with proper project structure

--- Week -2/Spring Core_Maven/Exercise 1: Configuring a Basic Spring Application/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.spring.exercise1;
-
-public class HelloWorld {
-
-    public void displayMessage() {
-        System.out.println("Hello, Welcome to Spring Framework!");
-    }
-
-}
