ee76c3dc635a03d4d2e2f6e4b73df773ff0d56dc
kaviyarsips <kavibhavi2006@gmail.com>
2026-07-07 22:07:54 +0530

    Delete Week -2/Spring Core_Maven/1. spring-data-jpa-handson/EmployeeRepository.java
    New file has updated with proper project structure

--- Week -2/Spring Core_Maven/1. spring-data-jpa-handson/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cognizant.spring_data_jpa_quick_example.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.cognizant.spring_data_jpa_quick_example.model.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-}
