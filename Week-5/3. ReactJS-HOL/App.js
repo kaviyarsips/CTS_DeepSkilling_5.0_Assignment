768262ebbd4aedf6538cab82862bde2ef84671f3
kaviyarsips <kavibhavi2006@gmail.com>
2026-07-22 22:31:39 +0530

    Add files via upload

---
+++ Week-5/3. ReactJS-HOL/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import CalculateScore from './Components/CalculateScore';
+
+function App() {
+
+  return (
+
+    <div>
+
+      <CalculateScore
+        name="Kaviyarasi"
+        school="Vel Tech High Tech Engineering College"
+        total={450}
+        goal={5}
+      />
+
+    </div>
+
+  );
+
+}
+
+export default App;
