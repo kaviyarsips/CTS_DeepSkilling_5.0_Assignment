96e82201678de865b8b2c41115e2dabd430e8007
kaviyarsips <kavibhavi2006@gmail.com>
2026-07-26 10:46:39 +0530

    Add files via upload
    Completed additional exercises

---
+++ Week-5/Additional_Exercise/6. ReactJS-HOL/TrainersMock.js
@@ -0,0 +1,43 @@
+import Trainer from "./trainer";
+
+const trainers = [
+
+    new Trainer(
+        1,
+        "John David",
+        "john@gmail.com",
+        "9876543210",
+        "React",
+        "React, JavaScript, HTML, CSS"
+    ),
+
+    new Trainer(
+        2,
+        "Peter",
+        "peter@gmail.com",
+        "9876543211",
+        "Angular",
+        "Angular, TypeScript"
+    ),
+
+    new Trainer(
+        3,
+        "Smith",
+        "smith@gmail.com",
+        "9876543212",
+        "Java",
+        "Java, Spring Boot"
+    ),
+
+    new Trainer(
+        4,
+        "Robert",
+        "robert@gmail.com",
+        "9876543213",
+        "Python",
+        "Python, Django"
+    )
+
+];
+
+export default trainers;
